d7149ecaf7b35fb9dcb0db4a7c9604430b97519b
keenalevine <keenalevine97@gmail.com>
2024-03-01 14:39:53 +0800

    feat: streaming json

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,5 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import * as fs from "fs";
+import { chain } from "stream-chain";
+
+import { parser } from "stream-json/Parser";
+import { pick } from "stream-json/filters/Pick";
+import { streamObject } from "stream-json/streamers/StreamObject";
+
 const mergeChunks = async (fileName: string, totalChunks: number) => {
   const chunkDir = process.cwd() + "/chunks";
   const mergedFilePath = process.cwd() + "/merged_files";
@@ -21,6 +27,26 @@ const mergeChunks = async (fileName: string, totalChunks: number) => {
   console.log("Chunks merged successfully");
 };
 
+const convertJson = (fileName: string) => {
+  const mergedFilePath = process.cwd() + "/merged_files";
+  const readStream = fs.createReadStream(`${mergedFilePath}/${fileName}`);
+  const pipeline = chain([
+    readStream,
+    parser(),
+    pick({ filter: "trades" }),
+    streamObject(),
+  ]);
+  pipeline.on("data", (data) => {
+    const permutations = JSON.parse(data.value);
+    //利用这些全部trades来计算东西
+    console.log(permutations.trades["BTCUSDT"].length);
+  });
+
+  pipeline.on("end", () =>
+    console.log(`The End`),
+  );
+};
+
 export async function POST(request: NextRequest) {
   const formData = await request.formData();
   const chunkIndex = Number(formData.get("chunkIndex"));
@@ -29,6 +55,7 @@ export async function POST(request: NextRequest) {
   const fileName = String(formData.get("filename"));
 
   const chunkDir = process.cwd() + "/chunks";
+  const mergedFilePath = process.cwd() + "/merged_files";
 
   if (!fs.existsSync(chunkDir)) {
     fs.mkdirSync(chunkDir);
@@ -37,12 +64,12 @@ export async function POST(request: NextRequest) {
   const chunkFilePath = `${chunkDir}/${fileName}.part_${chunkIndex}`;
 
   try {
-    await fs.writeFileSync(chunkFilePath, chunk);
+    await fs.promises.writeFile(chunkFilePath, chunk);
 
     if (chunkIndex === totalChunks - 1) {
       await mergeChunks(fileName, totalChunks);
     }
-
+    convertJson(fileName);
     return NextResponse.json({ message: "Chunk uploaded successfully" });
   } catch (error) {
     console.error("Error saving chunk:", error);
